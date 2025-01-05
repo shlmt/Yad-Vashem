a60b3dec059ad052d0a720826e12bd54c3712fc2
shlmt <sh7618sh@gmail.com>
2025-01-05 18:38:54 +0200

    filter by time in day

--- yad-vashem.client/src/app/components/times-screen/times-screen.component.ts
+++ yad-vashem.client/src/app/components/times-screen/times-screen.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TimeSlot } from '../../models/TimeSlot.interface';
 import { ApiService } from '../../api.service';
-import { Subscription } from 'rxjs';
+import { count, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-times-screen',
@@ -12,14 +12,21 @@ import { Subscription } from 'rxjs';
 })
 export class TimesScreenComponent implements OnInit, OnDestroy{
   private subscription: Subscription|undefined
-  public timeSlots: TimeSlot[] = []
-  public num = 0
+  timeSlots: TimeSlot[] = []
+  numOfCards = 0
+  num = 0
+  timeSlice:'morning'|'noon'|'afternoon'|'' = ''
+  startTime = 0
+  endTime = 24*60
 
   constructor(private apiService:ApiService) { }
 
   ngOnInit() {
     this.subscription = this.apiService.getTimeSlots().subscribe(data => {
-      if(data) this.timeSlots = data
+      if(data){
+        this.timeSlots = data
+        this.numOfCards = data.reduce((sum, item) => sum + (item.availablePlaces || 0), 0)
+      }
     })
   }
 
@@ -30,4 +37,37 @@ export class TimesScreenComponent implements OnInit, OnDestroy{
 
   inc=()=>this.num++
   dec=()=>this.num = Math.max(this.num-1,0)
+
+  chooseTime = (time:'morning'|'noon'|'afternoon'|'') => {
+    switch(time){
+      case 'morning' : {
+        this.timeSlice = 'morning'
+        this.startTime = this.convertToMinutes("8:00")
+        this.endTime = this.convertToMinutes("11:00")
+        break
+      }
+      case 'noon' : {
+        this.timeSlice = 'noon'
+        this.startTime = this.convertToMinutes("11:00")
+        this.endTime = this.convertToMinutes("13:00")
+        break
+      }
+      case 'afternoon' : {
+        this.timeSlice = 'afternoon'
+        this.startTime = this.convertToMinutes("13:00")
+        this.endTime = this.convertToMinutes("16:00")
+        break
+      }
+      default:{
+        this.startTime = 0
+        this.endTime = 24*60
+      }
+    }
+  }
+
+  convertToMinutes = (time:string) => {
+    const [hours, minutes] = time.split(":").map(Number)
+    return hours * 60 + minutes
+  }
+  
 }
